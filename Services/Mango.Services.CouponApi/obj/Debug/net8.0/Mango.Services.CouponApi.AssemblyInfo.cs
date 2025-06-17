83af4cc0262c91ce6f3153663a8520063fe3ad4e
nelliejellie <emekaewelike@gmail.com>
2025-06-17 15:43:23 +0100

    committed

--- Services/Mango.Services.CouponApi/obj/Debug/net8.0/Mango.Services.CouponApi.AssemblyInfo.cs
+++ Services/Mango.Services.CouponApi/obj/Debug/net8.0/Mango.Services.CouponApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2677495421db0db704328573e7bf20e82774d54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3107523e2c3c8851ebbfa870fdb24f9eead6d20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
